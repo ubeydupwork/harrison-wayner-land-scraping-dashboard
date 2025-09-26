99146c0a8da426fd07a11483d117e0ad6bcfba89
Ubeyd Gür <ubeydgur41@gmail.com>
2025-09-26 09:42:33 +0300

    ss

--- app.py
+++ app.py
@@ -37,7 +37,10 @@ counties = st.sidebar.multiselect("County", df["County"].unique())
 df["Price"] = df["Price"].replace('[\$,]', '', regex=True).astype(float)
 price_min = int(df["Price"].min())
 price_max = int(df["Price"].max())
+min_price_input = st.number_input("Min Price", min_value=price_min, max_value=price_max, value=price_min)
+max_price_input = st.number_input("Max Price", min_value=price_min, max_value=price_max, value=price_max)
 price_range = st.sidebar.slider("Price Range", price_min, price_max, (price_min, price_max))
+
 
 
 # Acres Range
